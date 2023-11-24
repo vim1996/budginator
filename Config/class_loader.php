5e321cd219f8d5f2dbe9a866ce846c8a7861a401
vim1996 <tmcva@hotmail.com>
2023-11-24 21:04:56 +0100

    test

--- Config/class_loader.php
+++ Config/class_loader.php
@@ -1,11 +1,12 @@
 <?php
 function class_loader($class_name) {
-    $class_file = __DIR__ . "/classes/" . $class_name . ".php";
+    $class_name = str_replace("\\", DIRECTORY_SEPARATOR, $class_name);
+    $class_file = __DIR__ . DIRECTORY_SEPARATOR . $class_name . ".php";
     
     if (file_exists($class_file)) {
         require_once $class_file;
     }
 }
 
-spl_autoload_register('class_loader');
+spl_autoload_register('class_loader');;
 ?>
